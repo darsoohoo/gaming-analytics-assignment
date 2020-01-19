777627c9f188671b7315df01e2b7ae199ea6139c
Darren SooHoo <darsoohoo@gmail.com>
2020-01-19 13:30:34 -0800

    Added production static assets for production build

--- server.js
+++ server.js
@@ -6,6 +6,17 @@ app.use(express.json({ extended: false }));
 
 app.use('/recommendations', require('./routes/recommendations'));
 
-const PORT = 5000;
-
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+  
+  const PORT = process.env.PORT || 5000;
+  
+  app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+  
