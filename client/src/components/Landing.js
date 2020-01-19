f24969fff39ce22c483e28570082d78e9cffbbd5
Darren SooHoo <darsoohoo@gmail.com>
2020-01-19 11:33:16 -0800

    Set the margin-top to 10px to decrease space between table and select dropdown

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -131,17 +131,24 @@ export class Landing extends Component {
 
     return (
       <section className="container landing">
-
-
         <div className='grouping-selection'>
-
-        <label>Group Select</label>
+        <label>Group by</label>
               <select className="browser-default" id="lang" onChange={(e) => this.change(e)} value={this.state.groupBy}>
                   <option value="Select">Select</option>
                   <option value="Clear">Clear</option>
-                  <option value="Bank">Bank</option>
-                  <option value="OldDenom">Old Denom</option>
+                  <option value="Asset">Asset</option>
+                  <option value="Action">Action</option>
+                  <option value="Status">Status</option>
+                  <option value="Area">Area</option>
                   <option value="Zone">Zone</option>
+                  <option value="Bank">Bank</option>
+                  <option value="Stand">Stand</option>
+                  <option value="NetWin">Net Win</option>
+                  <option value="OldDenom">Old Denom</option>
+                  <option value="NewDenom">New Denom</option>
+                  <option value="OldPaybackPct">Old Payback %</option>
+                  <option value="NewPaybackPct">New Payback %</option>
+                  <option value="Date">Date</option>
                </select>
         </div>
             <Table 
